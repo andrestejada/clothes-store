4f772264d5727df210048f59fff0a6b29b3b60e5
Andres <aneduardotejada@gmail.com>
2021-07-21 11:37:01 -0500

    sweet alert

--- src/components/Main/ProductsCarousel/CardProducts/CardProduct.js
+++ src/components/Main/ProductsCarousel/CardProducts/CardProduct.js
@@ -3,9 +3,18 @@ import './CardProduct.scss'
 import {ReactComponent as IconCart} from '../../../../assets/icon-cart.svg'
 import { addProduct } from '../../../../actions/shoppinCardAction'
 import { useDispatch } from 'react-redux'
+import Swal from 'sweetalert2'
 
 
 const CardProduct = ({nombre,precio,img}) => {
+    const handleAddProduct =()=>{
+        dispatch(addProduct({nombre,precio,img}))
+        Swal.fire({
+            title:'Producto agregado',
+            text:`${nombre} se agrego correctamente al carrito`,
+            icon:'success'
+        })
+    }
     const dispatch = useDispatch()
     return (
         <div className='card-producto-container' > 
@@ -14,7 +23,7 @@ const CardProduct = ({nombre,precio,img}) => {
                 <p>{nombre}</p>
                 <p className='price' >${precio}</p>
                 <button
-                    onClick={()=> dispatch(addProduct({nombre,precio,img})) }
+                    onClick={handleAddProduct}
                 > <IconCart/> Agregar al carrito</button>
             </div>
         </div>
